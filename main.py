abd7c8e9257d8b372aadcd395d6078e3cf431124
FakeEstateDeveloper <hormonalpokeman@gmail.com>
2026-02-15 03:23:17 +0800

    Edited steps in how_to.txt

--- main.py
+++ main.py
@@ -46,27 +46,3 @@ def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     raise HTTPException(status_code=404, detail=f"Item {item_id} was not found")
-
-# ACTIVATE
-# ====
-# ACTIVATE test API Command:
-# python -m uvicorn main:app --reload
-
-# RUN
-# ====
-# RUN the web so that others can access it:
-# source venv/bin/activate  # activate virtual environment
-# uvicorn main:app --reload --host 0.0.0.0 --port 8000
-
-# POST
-# ====
-# POST 'apple' item to list:
-# Invoke-RestMethod -Uri "http://127.0.0.1:8000/item?item=apple" -Method POST
-
-# GET
-# ====
-# GET index up to limit item from the list:
-# Invoke-RestMethod -Uri "http://127.0.0.1:8000/items?limit=3"
-
-# GET index '0''s item from the list:
-# Invoke-RestMethod -Uri "http://127.0.0.1:8000/items/0"
